5787cea45158aaff90140f8725d7282feae7cf2f
吕群 <893969029@qq.com>
2019-11-20 14:07:46 +0800

    上午写的

--- src/images/casarte.js
+++ src/images/casarte.js
@@ -19,3 +19,29 @@ var bannerSlider2 = $(".js_banner2").oSlider({
     pagerHover: false,
 });
 bannerSlider2.init();
+
+// 侧边栏
+$('.js_btnlist').on('mouseenter', '.js_user', function() {
+    var li = $(this);
+    li.find('b').addClass('displayblock');
+    li.find('span').addClass('colorwhite');
+    li.addClass('bca88747');
+});
+$('.js_btnlist').on('mouseleave', '.js_user', function() {
+    var li = $(this);
+    li.find('b').removeClass('displayblock');
+    li.find('span').removeClass('colorwhite');
+    li.removeClass('bca88747');
+});
+$('.js_btnlist').on('mouseenter', '.js_twocode', function() {
+    var twocode = $(this);
+    twocode.find('.cont').addClass('displayblock')
+});
+$('.js_btnlist').on('mouseleave', '.js_twocode', function() {
+    var twocode2 = $(this);
+    twocode2.find('.cont').removeClass('displayblock')
+});
+// 回到顶部
+$(".js_gototop_box").click(function() {
+    $("html,body").animate({ scrollTop: 0 }, 500);
+});
